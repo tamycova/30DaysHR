6fc5649977e5d1729acc5ec9f3690ee3d139b876
Samuel A. Winful <samuel@winful.com>
2016-01-06 12:04:46 -0500

    Forgot to uncomment stdin after my tests.

--- day6Stairs.tcl
+++ day6Stairs.tcl
@@ -3,8 +3,7 @@
 # The following line restarts using tclsh \
    exec tclsh "$0" ${1+"$@"}
 
-;#set width [gets stdin]
-set width 6
+set width [gets stdin]
 set stairCase {#}
 
 for {set i 0} { $i < $width } { incr i } {
