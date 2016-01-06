5fe5a672eacdd4c67f0ea13cf69ccacb26e5be4c
tamycova <tcovacevich@uc.cl>
2016-01-06 13:44:22 -0300

    merges

--- wittrup/day-2-arithmetic_Python_3.py
+++
@@ -1,8 +0,0 @@
-M = float(input())
-T = float(input())
-X = float(input())
-#M=12, T=20, X=8 
-tip = (T * M) / 100 #=2.4 
-tax = (X * M) / 100 #=0.96 
-final_price = M + tip + tax #=15.36 
-print("The final price of the meal is $%d." % round(final_price))
--- wittrup/day-5-loops_Python_3.py
+++
@@ -1,6 +0,0 @@
-for i in range(int(input())):
-    [a, b, N] = map(int, input().split())
-    for z in range(N):
-        a += 2**z * b
-        print( str(a) + ' ', end='')
-    print("")
