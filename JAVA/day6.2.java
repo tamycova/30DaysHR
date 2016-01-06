006f4f466ac1fd1e9a8354dd1b9e924168292305
Rohit Agrawal <e.rohit07@gmail.com>
2016-01-06 22:47:09 +0530

    Updated Code
    Without any loops; Much better performance.

--- JAVA/day6.2.java
+++ JAVA/day6.2.java
@@ -10,11 +10,11 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n - i - 1; j++)
-                System.out.print(" ");
-            for(int j = 0; j < i + 1; j++)
-                System.out.print("#");
-            System.out.println();
+            char[] spc = new char[n - i - 1];
+            Arrays.fill(spc, ' ');
+            char[] chr = new char[i + 1];
+            Arrays.fill(chr, '#');
+            System.out.println(new String(spc) + new String(chr));
         }
     }
 }
