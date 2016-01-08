dfed91b29ca81e0ab082c6ceb7963b295fe2301b
Gurupad Hegde <gshguru@gmail.com>
2016-01-08 05:37:51 +0530

    [python] day2: Fix to pass Test Case #3

--- PYTHON/day2.py
+++ PYTHON/day2.py
@@ -1,5 +1,5 @@
 orig = 10
 tip = 1
 tax = 3
-valor = orig + (orig * tip / 100) + (orig * tax / 100)
-print("The final price of the meal is $" + str(round(valor)) + ".")
+valor = orig + (orig * tip * 0.01) + (orig * tax * 0.01)
+print("The final price of the meal is $" + str(int(round(valor))) + ".")
