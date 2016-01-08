608a14a06ea4dfe86f88ce36da67067b794c6b11
leviathan88 <levigithuber@gmail.com>
2016-01-08 07:32:10 +0100

    beautifying day5

--- Scala/day5.scala
+++ Scala/day5.scala
@@ -2,15 +2,14 @@ object Solution {
     def main(args: Array[String]) {
         for (_ <- 0 until readInt)
         {
-           val arr = readLine.split(' ').map(_.toInt)
-           for (i <- 0 until arr(2)) {
-              var num = arr(0)
-              for (j <- 0 to i) {
-                   num += ((Math.pow(2,j)).toInt*arr(1))
-                    }
-                print(num+" ")
-                if(arr(2)-i==1)println
+            val arr = readLine.split(' ').toList.map(_.toInt)
+            var num = arr(0)
+                for (i <- 0 until arr(2)) {
+                    num += ((Math.pow(2,i)).toInt*arr(1))
+                    print(num+" ")
+                    if(arr(2)-i==1)println
             }
         }
     }
 }
+
