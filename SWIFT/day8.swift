d5c3e2ffe0359ef474cdcfd0ac2b45cca7810f80
swinful <swinful@users.noreply.github.com>
2016-01-08 13:56:35 -0500

    Updated day8.swift and removed excessive checks
    Optimized by removing excessive checks in the while loop.

--- SWIFT/day8.swift
+++ SWIFT/day8.swift
@@ -9,15 +9,10 @@ for _ in 1 ... numberOfEntries {
     phoneBook[name] = number
 }
 
-var possibleKey = readLine()
-while possibleKey != nil {
-
-  if let key = possibleKey {
-    if let foundKeyValue = phoneBook[key] {
+while let key = readLine() {
+  if let foundKeyValue = phoneBook[key] {
       print("\(key)=\(phoneBook[key]!)")
     } else {
       print("Not found")
-    }
   }
-  possibleKey = readLine()
 }
