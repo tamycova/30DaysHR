eb8c9feb73817793fa4339c74dfb996dd66d5945
Gurupad Hegde <gshguru@gmail.com>
2016-01-10 19:55:33 +0530

    [Java] Day 10 - Solution
    Using in-built function

---
+++ JAVA/day10.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Integer N = in.nextInt(); 
+        while (in.hasNext()) {
+                    System.out.println(Integer.toBinaryString(in.nextInt()));            
+        }
+
+    }
+}
