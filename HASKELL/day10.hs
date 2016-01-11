02a1ffa122e27e2833dce1ec907e319c518623fb
Runar Hovland <runar.hovland@gmail.com>
2016-01-10 23:49:40 -0800

    change concat . map to concatMap

--- HASKELL/day10.hs
+++ HASKELL/day10.hs
@@ -1,3 +1,3 @@
 main = interact $ unlines . map solve . drop 1 . lines
-solve = concat . map show . convert . read
+solve = concatMap show . convert . read
 convert = reverse . map (`mod` 2) . takeWhile (>0) . iterate (`div` 2)
