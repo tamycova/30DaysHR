b4ee34315263422523447d368b48722d55f993c4
della-90 <della90@msn.com>
2016-01-12 21:43:26 +0100

    Slightly changed some solutions

--- C/day3.c
+++ C/day3.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d", &N);
     if (N % 2 == 1 || (N % 2 == 0 && N <=20 && N >= 6)) { 
         printf("Weird");
-    } else if (N % 2 == 0 && N <= 5 && N >= 2 || N > 20) {
+    } else if (N % 2 == 0 && ((N <= 5 && N >= 2) || N > 20)) {
         printf("Not Weird");
     }
     return 0;
--- C/day7.c
+++ C/day7.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int N, nums[1000];
+    int N;
     scanf("%d", &N);
+    int nums[N];
     for (int N_i = N; N_i > 0; N_i--) scanf("%d ", &nums[N_i]);
     for (int N_i = 0; N_i < N; N_i++) printf("%d ", nums[N_i+1]);
     return 0;
