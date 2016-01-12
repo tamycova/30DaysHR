3585597f5c984bdef121f03fb864cea65b8a7202
Ashok Varma <ASHOKVITECE@GMAIL.COM>
2016-01-12 08:29:01 +0530

    Update day11.swift
    Code simplification and comments added

--- SWIFT/day11.swift
+++ SWIFT/day11.swift
@@ -1,22 +1,18 @@
 import Foundation
 
-var matrix: [[Int]] = [[]]
-for i in 0..<6 {
-    let line: [String] = readLine()!.characters.split { $0 == " "}.map(String.init)
-    matrix.append([]) // Create row
-    for elem in line {
-        matrix[i].append(Int(elem)!)
-    }   
+var matrix = [[Int]]()
+for _ in 0..<6 {
+    matrix.append(readLine()!.characters.split(" ").map(String.init).map{Int($0)!})
 }
 
 var result = Int.min
 for i in 1..<5 {
     for j in 1..<5 {
         // Swift wants me to split expression because "it's too complex"
-        var partial: Int = matrix[i][j]+matrix[i-1][j-1]
-        partial += matrix[i-1][j]+matrix[i-1][j+1]
-        partial += matrix[i+1][j-1]+matrix[i+1][j]
-        partial += matrix[i+1][j+1]
+        var partial = matrix[i-1][j-1] + matrix[i+1][j-1]    // first row first element and last row first element
+        partial += matrix[i-1][j] + matrix[i+1][j]    // first row second element and last row second element
+        partial += matrix[i-1][j+1] + matrix[i+1][j+1]    // first row third element and last row third element
+        partial += matrix[i][j]    // middle element
         if partial > result {
             result = partial
         }
