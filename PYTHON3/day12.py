59a27a04f54fdb2bb5d4da3cb196b3d4fd82e767
Shishir Gupta <ShishirG23@gmail.com>
2016-01-14 21:40:52 +0530

    More clear solution in Python3/day12.py

--- PYTHON3/day12.py
+++ PYTHON3/day12.py
@@ -3,15 +3,10 @@ class Grade(Student):
         super().__init__(fn, ln, p)
         self.score = score
     def calculate(self):
-        if self.score < 40:
-            return "D"
-        elif self.score >= 40 and self.score <60:
-            return "B"
-        elif self.score >= 60 and self.score<75:
-            return "A"
-        elif self.score >=75 and self.score <90:
-            return "E"
-        elif self.score >=90 and self.score <=100:
-            return "O"
+        if self.score < 40: return "D"
+        elif self.score<60: return "B"
+        elif self.score<75: return "A"
+        elif self.score<90: return "E"
+        else: return "O"
         
    
