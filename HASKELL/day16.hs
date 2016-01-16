347d3477c553e0569e7cc71668504eb8971fadee
Runar Hovland <runar.hovland@gmail.com>
2016-01-16 12:18:41 -0800

    remove ord constraint for diff.

--- HASKELL/day16.hs
+++ HASKELL/day16.hs
@@ -7,7 +7,7 @@ main = interact $ unwords . map show . minDiffList . sort . map read . drop 1 . 
 minDiffList :: (Num a,Ord a) => [a] -> [a]
 minDiffList = concatMap snd . head . groupBy ((==) `on` fst) . sort . diff
 
-diff :: (Num a,Ord a) => [a] -> [(a,[a])]
+diff :: Num a => [a] -> [(a,[a])]
 diff lst = zipWith f lst (tail lst)
     where f v1 v2 = (abs $ v1-v2,[v1,v2])
           
