5e1ed59e24986357e23d4eb1dae37ab044bf0f67
laxa <laxa@ddracepro.net>
2016-01-19 20:51:21 +0100

    Simplified day 19

--- C#/Day19/day19.cs
+++ C#/Day19/day19.cs
@@ -2,12 +2,12 @@ class Calculator : AdvancedArithmetic
 {
     public int divisorSum(int n)
     {
-        int ret = 0;
+        int ret = n;
         for (int i = 1; i <= n / 2; i++)
         {
             if (n % i == 0)
                 ret += i;
         }
-        return ret + n;
+        return ret;
     }
 }
