58fa83f8e4957b90d27a9b610159b7d090d935b2
Ashok Varma <ASHOKVITECE@GMAIL.COM>
2016-01-21 09:43:34 +0530

    Update Solution1.swift
    decreased timeouts still testCase 6 timedOut

--- SWIFT/Day20/Solution1.swift
+++ SWIFT/Day20/Solution1.swift
@@ -1,8 +1,9 @@
 func shouldSplit(char: Character ) -> Bool{
     let validationArray: Array<Character> = [" ","!","[",",","?",".","\\","_","\'","@","+","]"]
-    var result = false
-    validationArray.forEach{ result = (result || ($0==char)) }
-    return result
+    for current in validationArray{
+        if (current==char) { return true }
+    }
+    return false
 }
 var array = readLine()!.characters.split{ shouldSplit($0) }.map(String.init)
 print(array.count)
