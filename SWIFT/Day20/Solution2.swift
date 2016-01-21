f7b1b9f4b5f00591e23cbbd888c9bd95425a6f0a
Ashok Varma <ASHOKVITECE@GMAIL.COM>
2016-01-21 17:07:56 +0530

    Update Solution2.swift
    Working for all test cases

--- SWIFT/Day20/Solution2.swift
+++ SWIFT/Day20/Solution2.swift
@@ -1,10 +1,12 @@
+let validationArray: Array<Character> = [" ","!","[",",","?",".","\\","_","\'","@","+","]"]
+
 func shouldSplit(char: Character ) -> Bool{
-    let validationArray: Array<Character> = [" ","!","[",",","?",".","\\","_","\'","@","+","]"]
     for current in validationArray{
         if (current==char) { return true }
     }
     return false
 }
+
 var array = readLine()!.characters.split{ shouldSplit($0) }.map(String.init)
 print(array.count)
 print(array.joinWithSeparator("\n"));
