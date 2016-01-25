88b464989c2ffd6ab11d19d3e35d768be3cce796
Ashok Varma <ASHOKVITECE@GMAIL.COM>
2016-01-25 15:50:14 +0530

    Update Solution1.swift
    Removed unnecessary checks

--- SWIFT/Day24/Solution1.swift
+++ SWIFT/Day24/Solution1.swift
@@ -1,15 +1,13 @@
-if let validHead = head{
-    var temp: Node? = validHead
-    while let validTemp = temp {
-        if let nextNode = validTemp.next{
-            if(validTemp.data == nextNode.data){
-                validTemp.next = nextNode.next
-            }else{
-                temp = nextNode
+        var temp: Node? = head
+        while let validTemp = temp {
+            if let nextNode = validTemp.next{
+                if(validTemp.data == nextNode.data){
+                    validTemp.next = nextNode.next
+                }else{
+                    temp = nextNode
+                }
+            } else{
+                break
             }
-        } else{
-            temp = validTemp.next
         }
-    }
-}
-return head
+        return head
