8c6d83dc41d428cfdb70df5fd1032377eb26699b
Runar Hovland <runar.hovland@gmail.com>
2016-01-26 12:02:40 -0800

    Rename solution function

--- HASKELL/Day26/Solution1.hs
+++ HASKELL/Day26/Solution1.hs
@@ -1,5 +1,5 @@
-main = interact $ show . solveBase . map read . words
-solveBase [ad,am,ay,ed,em,ey]
+main = interact $ show . bookReturnFine . map read . words
+bookReturnFine [ad,am,ay,ed,em,ey]
   | ay<ey || (ay==ey && am<em) || (ay==ey && am==em && ad<=ed) = 0
   | ay==ey && am==em && ad>ed = 15*(ad-ed)
   | ay==ey && am>em = 500*(am-em)
