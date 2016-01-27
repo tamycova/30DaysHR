47c2aac5229b4b3fd2250a6d25799714a9dbc30c
laxa <laxa@ddracepro.net>
2016-01-27 20:03:39 +0100

    C# day27

---
+++ C#/Day27/Solution.cs
@@ -0,0 +1,20 @@
+using System;
+using static System.Console;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        WriteLine("5");
+        WriteLine("4 3");
+        WriteLine("-1 4 5 0");
+        WriteLine("3 2");
+        WriteLine("-1 5 0");
+        WriteLine("5 4");
+        WriteLine("-1 4 5 0 7");
+        WriteLine("6 1");
+        WriteLine("-1 4 5 0 3 3");
+        WriteLine("7 6");
+        WriteLine("-1 4 5 0 9 9 9");
+    }
+}
