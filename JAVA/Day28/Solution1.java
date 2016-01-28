2a4c0cc30553977d7d5974586bad0bed6b8f491e
Nicola <della-90@users.noreply.github.com>
2016-01-28 21:09:48 +0100

    Fixed #200

--- JAVA/Day28/Solution1.java
+++ JAVA/Day28/Solution1.java
@@ -6,7 +6,7 @@ public class Solution
    public static void main(String[] args){
       Scanner in = new Scanner(System.in);
       int testCases = Integer.parseInt(in.nextLine());
-      while(testCases>0){
+      while(testCases-->0){
          String pattern = in.nextLine();
          try {
              Pattern.compile(pattern);
