d835697f47bba886fc8b8b2024a5a779a7cf2f51
Misho Krastev <misho.kr+github@gmail.com>
2016-01-28 15:48:02 -0800

    [Ruby] Day 27 (improved)

--- RUBY/Day27/Solution1.rb
+++ RUBY/Day27/Solution1.rb
@@ -1,21 +1,29 @@
-earlies_arrival = -1000
-latest_arrival  =  1000
+MAX_N = 200
+MIN_N = 100
 
-max_n_tests  = 100
+earliest_arrival = -1000
+latest_arrival   =  1000
+
+max_n_tests  = rand((MIN_N-5)..(MAX_N-3))
+
 test_results = [true, false, true, false, true]
 test_results += (max_n_tests - test_results.length).times.map { rand(0..1) }.map { |x| x == 0 ? false : true }
 
+# N > 3 because there must be at least 3 arrivals -- positive, negative and zero
+all_ns = max_n_tests.times.map { |x| x + 3 }.shuffle
+
 tests = test_results.reverse.map do |is_positive|
-  
-  n = rand(3..20)
+
+  # N must be uniquie across all tests
+  n = all_ns.pop
   k = rand(1..(n-2))
-  
+
   to_be_early = is_positive ? rand(k..n) : rand(1..k)
   to_be_late  = n - to_be_early
 
-  early = to_be_early.times.map { rand(earlies_arrival..0) }
-  late  = to_be_late.times.map  { rand(1..latest_arrival)  }
-  
+  early = to_be_early.times.map { rand(earliest_arrival..0) }
+  late  = to_be_late.times.map  { rand(1..latest_arrival)   }
+
   early[0] = 0 if early.index(0).nil?
 
   # it is just too complicated to correct the test to match the requirements,
